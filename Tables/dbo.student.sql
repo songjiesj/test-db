6e4134340bf94fb917a198d429fcbe93f02f459d
songjiesj <18811316256@163.com>
2022-07-05 11:26:47 +0800

    add new table called student

---
+++ Tables/dbo.student.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[student]
+(
+[Name] [varchar] (5) COLLATE Chinese_PRC_CI_AS NOT NULL,
+[Age] [int] NULL,
+[School] [varchar] (5) COLLATE Chinese_PRC_CI_AS NULL,
+[Class] [varchar] (5) COLLATE Chinese_PRC_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[student] ADD CONSTRAINT [PK__student__737584F75756E1E8] PRIMARY KEY CLUSTERED ([Name]) ON [PRIMARY]
+GO
