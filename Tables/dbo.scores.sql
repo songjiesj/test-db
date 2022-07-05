20df8cfefc0703f2323e23e879fbb5e65b469ef6
songjiesj <18811316256@163.com>
2022-07-05 11:22:55 +0800

    delete tables

--- Tables/dbo.scores.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[scores]
-(
-[Name] [varchar] (10) COLLATE Chinese_PRC_CI_AS NOT NULL,
-[Subject] [varchar] (10) COLLATE Chinese_PRC_CI_AS NULL,
-[Score] [int] NULL,
-[Rank] [int] NULL
-) ON [PRIMARY]
-GO
--- Tables/dbo.student.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[student]
-(
-[Name] [varchar] (10) COLLATE Chinese_PRC_CI_AS NOT NULL,
-[Age] [int] NOT NULL,
-[Class] [varchar] (10) COLLATE Chinese_PRC_CI_AS NULL
-) ON [PRIMARY]
-GO
