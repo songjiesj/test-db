fabf3b539df3d113a165266a450854c8b7452001
songjiesj <18811316256@163.com>
2022-07-05 12:07:41 +0800

    update score data

--- Data/dbo.score_Data.sql
+++ Data/dbo.score_Data.sql
@@ -1,2 +1,3 @@
+INSERT INTO [dbo].[score] ([Name], [Class], [Subject], [Score], [Rank]) VALUES (N'李四', N'一7', N'math', 112, 1)
 INSERT INTO [dbo].[score] ([Name], [Class], [Subject], [Score], [Rank]) VALUES (N'宋杰', N'一9', N'math', 100, 3)
 INSERT INTO [dbo].[score] ([Name], [Class], [Subject], [Score], [Rank]) VALUES (N'张三', N'一10', N'math', 102, 2)
--- Data/dbo.student_Data.sql
+++ Data/dbo.student_Data.sql
@@ -1,2 +1,3 @@
-INSERT INTO [dbo].[student] ([Name], [Age], [School], [Class]) VALUES (N'张三', 25, N'No.8', N'一10')
+INSERT INTO [dbo].[student] ([Name], [Age], [School], [Class]) VALUES (N'李四', 26, N'No.8', N'一7')
 INSERT INTO [dbo].[student] ([Name], [Age], [School], [Class]) VALUES (N'宋杰', 25, N'No.8', N'一9')
+INSERT INTO [dbo].[student] ([Name], [Age], [School], [Class]) VALUES (N'张三', 25, N'No.8', N'一10')
